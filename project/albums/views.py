a182b9df7e2c89eb84cec7f926d984ceaf976ad1
marcangelvillarosa <marcangel.villarosa@hcdc.edu.ph>
2024-09-30 22:09:57 +0800

    gege

---
+++ project/albums/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Tag
+from .serializers import TagSerializer
+
+class TagListCreateView(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
